e68d990259b3aa067c86dd68528cbca48f8b02e2
Damilare <opeyemisamuel222@gmail.com>
2021-01-24 15:38:43 +0000

    getting the first row larger

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import Row from './Row';
 import request from './request'
+import './App.css'
 
 const App = () => {
   return (
-    <div>
-      <Row title='NETFLIX ORIGINALS' fetchUrl= {request.fetchNetflixOriginals} />
-      <Row title='Trending Now' fetchUrl= {request.fetchTrending} />
+    <div className="App">
+      <Row title="NETFLIX ORIGINALS" fetchUrl={request.fetchNetflixOriginals} isLargeRow />
+      <Row title="Trending Now" fetchUrl={request.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={request.fetchTrending} />
+      <Row title="Action Movies" fetchUrl={request.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={request.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={request.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={request.fetchRomanceMovies} />
+      <Row title="Documemaries" fetchUrl={request.fetchDocumentariesMovies} />
     </div>
-  )
+  );
 }
 
 export default App
--- src/Row.js
+++ src/Row.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
+import "./Row.css";
 
-const Row = ({ title, fetchUrl }) => {
+const baseUrl = "https://image.tmdb.org/t/p/original/";
+
+const Row = ({ title, fetchUrl, isLargeRow }) => {
   const [movies, setmovies] = useState([]);
 
   useEffect(() => {
@@ -13,8 +16,26 @@ const Row = ({ title, fetchUrl }) => {
 
     fetchData();
   }, [fetchUrl]);
+  console.log(movies);
 
-  return <div></div>;
+  const renderedMovies = movies.map((movie) => {
+    return (
+      <img
+        key={movie.id}
+        className={`row__poster ${isLargeRow && 'row__posterLarge'}`}
+        src={`${baseUrl}${
+          isLargeRow ? movie.poster_path : movie.backdrop_path
+        }`}
+        alt={movie.name}
+      />
+    );
+  });
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row__posters">{renderedMovies}</div>
+    </div>
+  );
 };
 
 export default Row;
